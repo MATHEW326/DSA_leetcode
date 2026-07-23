59a1de41121e206c1a195178aa2271e257fa6f7b
MATHEW <2300032271@kluniversity.in>
2026-07-23 23:07:06 +0530

    recursion

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.py
@@ -1,11 +1,14 @@
 class Solution:
     def minCostClimbingStairs(self, cost: List[int]) -> int:
-        n=len(cost)
-        dp=[0,0]    
-        for i in range(2,n+1):
-            dp.append(min(dp[i-2]+cost[i-2],dp[i-1]+cost[i-1]))
-        return dp[n]
-
+        store={}
+        def rec(i):
+            if i==0 or i==1:
+                return 0
+            if i in store:
+                return store[i]
+            store[i]=min(rec(i-1)+cost[i-1],rec(i-2)+cost[i-2])
+            return store[i]
+        return rec(len(cost))
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4

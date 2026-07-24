c0cc7beb93d8fc846bd8d61117cbb679fdb37997
MATHEW <2300032271@kluniversity.in>
2026-07-24 14:49:47 +0530

    dictionary_method

--- 0136-single-number/0136-single-number.py
+++ 0136-single-number/0136-single-number.py
@@ -1,9 +1,13 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
-        x=0
-        for i in range(len(nums)):
-            x=x^nums[i]
-        return x
+        dictionary={}
+        x=-1
+        for i in nums:
+            dictionary[i]=dictionary.get(i,0)+1
+        for k,v in dictionary.items():
+            if v==1:
+                return k
+        return -1
 
 # Synced seamlessly with LeetHub Pro
 # Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
